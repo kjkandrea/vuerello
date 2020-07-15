835a0c161efcaa03e4d155f977cd910bcd1c4b9a
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-15 18:53:35 +0900

    vue-router : fetch 더미 메소드 구현

--- src/components/board/ComponentCard.vue
+++ src/components/board/ComponentCard.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     Card
-    <div>cid: {{cid}}</div>
+    <div v-if="loading">Loading Card...</div>
+    <div v-else>cid: {{cid}}</div>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    cid() {
-      return this.$route.params.cid
+  data() {
+    return {
+      cid: 0,
+      loading: false
+    }
+  },
+  watch: {
+    '$route': {
+      handler: 'fetchData',
+      immediate: true
+    }
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.cid = this.$route.params.cid
+        this.loading = false
+      }, 500)
     }
   }
 }
