a6c5592f7c2563eb180e8deebb103265b6ca77a7
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-16 18:32:49 +0900

    네비게이션 가드 설정 : 토큰 유무

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -8,16 +8,27 @@ import NotFound from '../components/PageNotFound'
 
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem('token')
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth ? next() : next(loginPath)
+}
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
+    { path: '/', component: Home, beforeEnter: requireAuth() },
     { path: '/login', component: Login },
     { 
-      path: '/b/:bid',
+      path: '/b/:bid', 
       component: Board,
+      beforeEnter: requireAuth(),
       children : [
-        { path: 'c/:cid', component: Card }
+        { 
+          path: 'c/:cid', 
+          component: Card,
+          beforeEnter: requireAuth()
+        }
       ]
     },
     { path: '*', component: NotFound }
