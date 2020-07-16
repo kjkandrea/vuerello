395751da2a47b9d3558231808178fbabd96be584
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-16 17:26:28 +0900

    컴포넌트의 axios 의존성 제거

--- client/src/components/PageHome.vue
+++ client/src/components/PageHome.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import {board} from '../api'
 
 export default {
   data() {
@@ -38,16 +38,13 @@ export default {
     fetchData() {
       this.loading = true
 
-      axios.get('http://localhost:3000/boards')
-        .then(res => {
-          this.boards = res.data
-        })
-        .catch(res => {
-          this.$router.replace('/login')
-        })
-        .finally(() => {
-          this.loading = false
-        })
+      board.fetch()
+       .then(data => {
+         this.boards = data
+       })
+      .finally(() => {
+        this.loading = false
+      })
     }
   }
 }
