bb6e771831afda5d7840dcd1feaa323d06afe51d
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-19 18:20:37 +0900

    publisher : card 퍼블리싱을 가져옴

--- client/src/components/board/ComponentCard.vue
+++ client/src/components/board/ComponentCard.vue
@@ -1,10 +1,19 @@
 <template>
-  <modal>
-    <div slot="body">
-      {{ card }}
+  <modal class="modal-card">
+    <div slot="header" class="modal-card-header">
+      <div class="modal-card-header-title">
+        <input class="form-control" type="text" :value="card.title" readonly>
+      </div>
+      <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
     </div>
-    <div v-if="loading">Loading Card...</div>
-    <div v-else>cid: {{cid}}</div>
+    <div slot="body">
+      {{card}}
+      <h3>Description</h3>
+      <textarea  class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
+        readonly
+        v-model="card.description"></textarea>
+    </div>
+    <div slot="footer"></div>
   </modal>
 </template>
 
@@ -18,7 +27,8 @@ export default {
   },
   computed: {
     ...mapState({
-      card: 'card'
+      card: 'card',
+      board: 'board'
     })
   },
   data() {
@@ -34,7 +44,31 @@ export default {
   methods: {
     ...mapActions([
       'FETCH_CARD'
-    ])
+    ]),
+    onClose() {
+      this.$router.push(`/b/${this.board.id}`)
+    }
   }
 }
 </script>
+
+<style>
+.modal-card .modal-container {
+  min-width: 300px;
+  max-width: 800px;
+  width: 60%;
+}
+.modal-card-header-title {
+  padding-right: 30px;  
+}
+.modal-close-btn {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  font-size: 24px;
+  text-decoration: none;
+}
+.modal-card-header {
+  position: relative;
+}
+</style>
