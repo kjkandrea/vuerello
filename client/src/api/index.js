a63498774816f3a9b39eec9ce3767b29cde12d77
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-19 18:54:24 +0900

    update card 구현

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -25,6 +25,12 @@ export const setAuthInHeader = token => {
   axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null
 }
 
+export const auth = {
+  login(email, password) {
+    return request('post', '/login', {email, password})
+  }
+}
+
 export const board = {
   fetch(id) {
     return id ? request('get', `/boards/${id}`) : request('get', '/boards')
@@ -34,17 +40,14 @@ export const board = {
   }
 }
 
-export const auth = {
-  login(email, password) {
-    return request('post', '/login', {email, password})
-  }
-}
-
 export const card = {
   create(title, listId, pos) {
     return request('post', '/cards', { title, listId, pos})
   },
   fetch(id) {
     return request('get', `/cards/${id}`)
+  },
+  update(id, payload) {
+    return request('put', `/cards/${id}`, payload)
   }
 }
